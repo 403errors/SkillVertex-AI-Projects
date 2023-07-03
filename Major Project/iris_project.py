6e6c1956dfe879d77fed85568ee24dd96091858d
Saitama_IITM <95044569+Sitama-from-IITM@users.noreply.github.com>
2023-07-03 10:40:37 +0530

    modify

--- Major Project/iris_project.py
+++ Major Project/iris_project.py
@@ -46,31 +46,29 @@ url="https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
-# #______________________________________________________Optional__________________________________________________________
-# # shape
-# print(dataset.shape)
+# shape
+print(dataset.shape)
 
-# # head
-# print(dataset.head(20))
+# head
+print(dataset.head(20))
 
-# # descriptions
-# print(dataset.describe())
+# descriptions
+print(dataset.describe())
 
-# # class distribution
-# print(dataset.groupby('class').size())
+# class distribution
+print(dataset.groupby('class').size())
 
-# # box and whisker plots
-# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-# pyplot.show()
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
 
-# # histograms
-# dataset.hist()
-# pyplot.show()
+# histograms
+dataset.hist()
+pyplot.show()
 
-# # scatter plot matrix
-# scatter_matrix(dataset)
-# pyplot.show()
-# #______________________________________________________Optional_________________________________________________________
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
 
 # Split-out test dataset
 array = dataset.values
@@ -78,30 +76,28 @@ X = array[:,0:4]
 y= array[:,4]
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.20, random_state = 1, shuffle=True)
 
-# #_____________________________________________________Optional_____________________________________________________________
-# # Spot Check Algorithms
-# models = []
-# models.append(('LR',LogisticRegression(solver='liblinear',multi_class= 'ovr')))
-# models.append(('KNN', KNeighborsClassifier()))
-# models.append(('CART', DecisionTreeClassifier()))
-# models.append(('NB', GaussianNB()))
-# models.append (('SVM', SVC(gamma='auto')))
+# Spot Check Algorithms
+models = []
+models.append(('LR',LogisticRegression(solver='liblinear',multi_class= 'ovr')))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append (('SVM', SVC(gamma='auto')))
 
-# # evaluate each model in turn
-# results = []
-# names = []
-# for name, model in models:
-#   kfold = StratifiedKFold(n_splits=10, random_state=1,shuffle =True)
-#   cv_results = cross_val_score(model, X_train. Y_train, cv=kfold,scoring= 'accuracy')
-#   results.append(cv_results)
-#   names.append(name)
-# print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+  kfold = StratifiedKFold(n_splits=10, random_state=1,shuffle =True)
+  cv_results = cross_val_score(model, X_train. Y_train, cv=kfold,scoring= 'accuracy')
+  results.append(cv_results)
+  names.append(name)
+print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
 # # Compare Algorithms
-# pyplot.boxplot(results, labels=names)
-# pyplot.title('Algorithm Comparison')
-# pyplot.show()
-# #_____________________________________________________Optionnal____________________________________________________________
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
 
 # Make predictions on validation dataset
 model = SVC(gamma='auto')
